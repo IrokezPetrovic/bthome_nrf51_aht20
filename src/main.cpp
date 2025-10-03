9bab8c18b7efec6578cc723a289bdd444e2f2997
Paul Granov <p.granov@scloud.ru>
2025-10-03 17:41:40 +0300

    Reboot by WDT

--- src/main.cpp
+++ src/main.cpp
@@ -20,35 +20,31 @@ void setup() {
     pinMode(21, INPUT_PULLUP);
     attachInterrupt(21, &onBtn, FALLING);    
     
-    NVIC_EnableIRQ(RTC0_IRQn);
-    //NRF_RTC0->POWER = 1;
-    NRF_RTC0->PRESCALER = 3276;
-    NRF_RTC0->CC[1] = 50;
-    NRF_RTC0->EVTEN = 0x1<<17;    
-    NRF_RTC0->INTENSET = 1<<17;    
-    NRF_RTC0->TASKS_START = 1;    
-    init();
+    NRF_WDT->CONFIG = (WDT_CONFIG_HALT_Pause << WDT_CONFIG_HALT_Pos) | ( WDT_CONFIG_SLEEP_Run << WDT_CONFIG_SLEEP_Pos);   //Configure Watchdog. a) Pause watchdog while the CPU is halted by the debugger.  b) Keep the watchdog running while the CPU is sleeping.
+	  NRF_WDT->CRV = 2*32768;             //ca 3 sek. timout
+	  NRF_WDT->RREN |= WDT_RREN_RR0_Msk;  //Enable reload register 0
+	  NRF_WDT->TASKS_START = 1;           //Start the Watchdog timer
 }
 
-int i = 0;
-void loop() {  
+
+void loop() {
   digitalWrite(22, HIGH);
-  delay(100);
+  delayMicroseconds(100000);
   digitalWrite(22, LOW);
-  delay(100);
-  __WFE();
-  __SEV();
-  __WFE();            
+  delayMicroseconds(100000);
+
+  // __WFE();
+  // __SEV();
+  // __WFE();            
+  __WFI();
 }
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 void RTC0_IRQHandler(void){
-  //NRF_RTC0->TASKS_STOP= 1;
-  NRF_RTC0->TASKS_CLEAR= 1;
-  //NRF_RTC0->TASKS_START= 1;  
-  NRF_RTC0->EVENTS_COMPARE[1] = 0;
+  // NRF_RTC0->TASKS_CLEAR= 1;
+  // NRF_RTC0->EVENTS_COMPARE[1] = 0;
   
 }
 #ifdef __cplusplus
